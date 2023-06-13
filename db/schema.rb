58036cbf8da34416547f6fd9a8e027c4cf8bae6d
Stanley <78428936+chepkok3@users.noreply.github.com>
2023-06-13 18:01:05 +0300

    Reset db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_13_121629) do
   enable_extension "plpgsql"
 
   create_table "greetings", force: :cascade do |t|
+    t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
